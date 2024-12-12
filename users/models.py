ac1bd9212e7f9ee8a7211cb3a8f908fe1fd5fcfa
cam lowndes <cameronlowndes@gmail.com>
2024-12-12 12:26:12 +0000

    working on email

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # Creates a one-to-one relationship with the User model
@@ -11,3 +12,10 @@ class Profile(models.Model):
     # You can add a method to handle image resizing if required in the future
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
